4e3bcfad22b8ed803027c78f69d5e5f719262cda
Joy-Amorin <amorinkaren2021@gmail.com>
2023-08-22 14:07:12 -0700

    remove count change variable´s name

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,14 +1,7 @@
-#!/usr/bin/python3
-
-"""Write a Python script that, using this REST API,
-for a given employee ID, returns
-information about his/her TODO list progress."""
-
 import requests
 from sys import argv
 
 if __name__ == "__main__":
-
     n = int(argv[1])
 
     res_user = requests.get(f"https://jsonplaceholder.typicode.com/users/{n}")
@@ -17,22 +10,19 @@ if __name__ == "__main__":
     res_tasks = requests.get(f"https://jsonplaceholder.typicode.com/todos")
     data_tasks = res_tasks.json()
 
-    _tasks = 0
-    realized = 0
     completed_tasks = []
+    total_tasks = 0
+    done_tasks = 0
 
-    for tasks in data_tasks:
-        if tasks["userId"] == n:
-            if tasks["completed"] is True:
-                completed_tasks.append(tasks["title"])
-                realized += 1
+    for task in data_tasks:
+        if task["userId"] == n:
+            total_tasks += 1
+            if task["completed"]:
+                done_tasks += 1
+                completed_tasks.append(task["title"])
 
-            else:
-                _tasks += 1
+    print(f"Employee {data_user['name']} has completed tasks "
+          f"({done_tasks}/{total_tasks}):")
 
-            _tasks += realized
-
-    print(f"Employee {data_user['name']} is done with tasks"
-          f"({realized}/{_tasks}):")
     for task_title in completed_tasks:
         print(f"\t {task_title}")
